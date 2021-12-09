7d0af2806eef30e6921f3df75ceb61c7c34f1be0
maxleclerc2 <mleclerc@et.esiea.fr>
2021-12-09 19:37:13 +0100

    Fixed PingControllerIT, was not built by Maven

--- prediction-engine/src/test/java/fr/lernejo/prediction/PingControllerIT.java
+++ prediction-engine/src/test/java/fr/lernejo/prediction/PingControllerIT.java
@@ -1,5 +1,7 @@
 package fr.lernejo.prediction;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,13 +9,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(classes = Launcher.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 class PingControllerIT {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new PingController()).build();
+    }
+
     @Test
-    void ping_respond_OK(@Autowired MockMvc mockMvc) throws Exception {
+    void ping_respond_OK() throws Exception {
 
         mockMvc
             .perform(MockMvcRequestBuilders.get("/api/ping"))
